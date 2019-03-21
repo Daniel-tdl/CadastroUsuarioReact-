6aaacc1d5a1a79aa64f04775522b050e51fae0f4
Daniel_jr <daniel@extradigital.com.br>
2019-03-21 18:59:15 -0300

    Configurando o footer da aplicação.

--- crud/frontend/src/components/template/Footer.jsx
+++ crud/frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span className="footer">
+            Desenvolvido com <i className="fa fa-heart text-danger" > </i> por 
+            <strong> Daniel Coutinho </strong>
+        </span>
     </footer>
